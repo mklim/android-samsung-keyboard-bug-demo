e0805a15ca293a4dd7cfb3ec1d1c624db6f2ed0a
Michael Klimushyn <mklim@google.com>
2019-09-20 11:06:23 -0700

    Workaround demo.

--- app/src/main/java/com/example/samsunginputbug/MainActivity.java
+++ app/src/main/java/com/example/samsunginputbug/MainActivity.java
@@ -3,19 +3,24 @@ package com.example.samsunginputbug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
     }
 
     public void onClick(View view) {
-        EditText editText = findViewById(R.id.editText);
+        InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
         editText.setSelection(0);
+        ic.finishComposingText();
     }
 }
